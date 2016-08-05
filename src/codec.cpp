c07141a07489fce9a5ae317ad8555ab88d3a7e16
Javier Valcarce <javier.valcarce@gmail.com>
2016-08-05 23:35:46 +0200

    Codec is working now

--- src/codec.cpp
+++ src/codec.cpp
@@ -2,6 +2,7 @@
 #include "codec.h"
 
 #include <cassert>
+#include <cstdio>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
@@ -16,7 +17,7 @@ int fpga_link1::Encoder(Command& cmd, SerializedCommand* serialized) {
       
       switch (cmd.type) {
       case kWrite8:
-            tmp[0] = cmd.type << 4;
+            tmp[0] = cmd.type;
             tmp[1] = (cmd.address & 0x00ff0000) >> 16;
             tmp[2] = (cmd.address & 0x0000ff00) >> 8;
             tmp[3] = (cmd.address & 0x000000ff) >> 0;
@@ -24,11 +25,11 @@ int fpga_link1::Encoder(Command& cmd, SerializedCommand* serialized) {
             tmp[5] = 0x00;
             tmp[6] = 0x00;
             tmp[7] = 0x00;
-            tmp[5] = 0xFF; // CRC
+            tmp[8] = 0xFF; // CRC
             break;
           
       case kWrite32:
-            tmp[0] = cmd.type << 4;
+            tmp[0] = cmd.type;
             tmp[1] = (cmd.address & 0x00ff0000) >> 16;
             tmp[2] = (cmd.address & 0x0000ff00) >> 8;
             tmp[3] = (cmd.address & 0x000000ff) >> 0;
@@ -36,12 +37,23 @@ int fpga_link1::Encoder(Command& cmd, SerializedCommand* serialized) {
             tmp[5] = (cmd.data32 & 0x00ff0000) >> 16;
             tmp[6] = (cmd.data32 & 0x0000ff00) >> 8;
             tmp[7] = (cmd.data32 & 0x000000ff) >> 0;
-            tmp[5] = 0xFF; // CRC
+            tmp[8] = 0xFF; // CRC
             break;
       default:
             assert(false);
       }
 
+      printf("Non-encoded Frame: 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n",
+             tmp[0],
+             tmp[1],
+             tmp[2],
+             tmp[3],
+             tmp[4],
+             tmp[5],
+             tmp[6],
+             tmp[7],
+             tmp[8]);
+      
       serialized->size = 10;
       
       // First transmitted byte over serial link is serialized->data[0]
@@ -54,11 +66,25 @@ int fpga_link1::Encoder(Command& cmd, SerializedCommand* serialized) {
       serialized->data[4] = ((tmp[3] & 0b00000011) << 5) | ((tmp[4] & 0b11111000) >> 3);  // 2+5
       serialized->data[3] = ((tmp[2] & 0b00000001) << 6) | ((tmp[3] & 0b11111100) >> 2);  // 1+6
       serialized->data[2] = ((tmp[1] & 0b00000000) << 7) | ((tmp[2] & 0b11111110) >> 1);  // 0+7
+      
       serialized->data[1] = ((tmp[1] & 0b01111111) << 0) | ((tmp[2] & 0b00000000) >> 0);  // 7+0
       serialized->data[0] = ((tmp[0] & 0b00111111) << 1) | ((tmp[1] & 0b10000000) >> 7);  // 6+1
 
       // First byte of encoded frame has its MSB set to '1', all others set to '0'.
       serialized->data[0] |= 0b10000000;
+
+      printf("Encoded Frame    : 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n",
+             serialized->data[0],
+             serialized->data[1],
+             serialized->data[2],
+             serialized->data[3],
+             serialized->data[4],
+             serialized->data[5],
+             serialized->data[6],
+             serialized->data[7],
+             serialized->data[8],
+             serialized->data[9]);
+
       
       return 0;
 }
@@ -66,7 +92,43 @@ int fpga_link1::Encoder(Command& cmd, SerializedCommand* serialized) {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int fpga_link1::Decoder(Command& cmd, SerializedCommand* serialized) {
+
+      uint8_t tmp[9];
+
+      assert(serialized->size == 10);
       
+      tmp[8] = ((serialized->data[8] & 0b00000001) << 7) | ((serialized->data[9] & 0b01111111) >> 0);
+      tmp[7] = ((serialized->data[7] & 0b00000011) << 6) | ((serialized->data[8] & 0b01111110) >> 1);
+      tmp[6] = ((serialized->data[6] & 0b00000111) << 5) | ((serialized->data[7] & 0b01111100) >> 2);
+      tmp[5] = ((serialized->data[5] & 0b00001111) << 4) | ((serialized->data[6] & 0b01111000) >> 3);
+      tmp[4] = ((serialized->data[4] & 0b00011111) << 3) | ((serialized->data[5] & 0b01110000) >> 4);
+      tmp[3] = ((serialized->data[3] & 0b00111111) << 2) | ((serialized->data[4] & 0b01100000) >> 5);      
+      tmp[2] = ((serialized->data[2] & 0b01111111) << 1) | ((serialized->data[3] & 0b01000000) >> 6);
+      tmp[1] = ((serialized->data[0] & 0b00000001) << 7) | ((serialized->data[1] & 0b01111111) >> 0);
+      tmp[0] = ((serialized->data[0] & 0b00111110) >> 1);
+
+      printf("Non-encoded Frame: 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n",
+             tmp[0],
+             tmp[1],
+             tmp[2],
+             tmp[3],
+             tmp[4],
+             tmp[5],
+             tmp[6],
+             tmp[7],
+             tmp[8]);
+
+      switch (tmp[0]) {
+      case kWrite32:
+            cmd.type = static_cast<CommandType>(tmp[0]);
+            cmd.address = tmp[1] << 16 | tmp[2] <<  8 | tmp[3] << 0;
+            cmd.data32  = tmp[4] << 24 | tmp[5] << 16 | tmp[6] << 8 | tmp[7] << 0;
+            // CRC TODO
+            break;
+      default:
+            assert(false);
+      }
+
       return 0;
 }
 
--- src/test_codec.cpp
+++ src/test_codec.cpp
@@ -16,11 +16,11 @@ int main(int argc, char** argv) {
       //fpga_link1::SerializedCommand ser2;
       
       cmd1.type = fpga_link1::kWrite32;
-      cmd1.address = 0x00223344;
-      cmd1.data32 = 0xAABBCCDD;
+      cmd1.address = 0x00253545;
+      cmd1.data32 = 0xA5B5C5D5;
 
       fpga_link1::Encoder(cmd1, &ser);
-
+/*
       printf("0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n", 
              ser.data[0],
              ser.data[1],
@@ -32,9 +32,18 @@ int main(int argc, char** argv) {
              ser.data[7],
              ser.data[8],
              ser.data[9]);
-
+*/
       
       assert((ser.data[0] & 0x80) == 0x80);
+      assert((ser.data[1] & 0x80) == 0x00);
+      assert((ser.data[2] & 0x80) == 0x00);
+      assert((ser.data[3] & 0x80) == 0x00);
+      assert((ser.data[4] & 0x80) == 0x00);
+      assert((ser.data[5] & 0x80) == 0x00);
+      assert((ser.data[6] & 0x80) == 0x00);
+      assert((ser.data[7] & 0x80) == 0x00);
+      assert((ser.data[8] & 0x80) == 0x00);
+      assert((ser.data[9] & 0x80) == 0x00);
 
 
       fpga_link1::Decoder(cmd2, &ser);
